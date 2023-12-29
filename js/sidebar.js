11b3c05ec50d7d353abffcf413f7694be3421bc2
bidursapkota00 <bidursapkota00@gmail.com>
2023-12-29 13:45:08 +0545

    hamburger added

---
+++ js/sidebar.js
@@ -0,0 +1,12 @@
+const hamburgerSection = document.getElementById('hamburger');  // Main container of hamburger section
+const hamburgerLines = document.getElementsByClassName("hamburger__lines__section")[0];  // Container of Lines
+const hamburgerLine = document.getElementsByClassName("hamburger__line"); // Array of Lines
+
+hamburgerLines.addEventListener('click', function () {
+    hamburgerSection.classList.toggle('sidebar__hamburger__translate');
+    for (let i = 0; i < hamburgerLine.length; i++) {
+        i == 0 && hamburgerLine[i].classList.toggle('hamburger__line1__onhover')
+        i == 2 && hamburgerLine[i].classList.toggle('hamburger__line3__onhover')
+        hamburgerLine[i].classList.toggle('hamburger__line__cross')
+    }
+});
